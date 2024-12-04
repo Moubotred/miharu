819c37feebf1b5add7ca3f617e650a951fa1a1e1
Moubotred <fakesarnamer@gmail.com>
2024-12-03 22:32:44 -0500

    modificacin de directorios

---
+++ apis/test/test_luzdelsur.py
@@ -0,0 +1,13 @@
+import pytest
+from apis.Luzdelsur import LuzdelsurRecibo
+
+@pytest.mark.asyncio
+async def test_LuzdelsurRecibo_simple():
+    # Número de suministro de prueba
+    suministro = "7998"  # Este número debe ser válido en la API
+    
+    # Llamar a la función
+    resultado = await LuzdelsurRecibo(suministro)
+    
+    # Verificar si se devuelve el archivo esperado
+    # assert resultado == f"{suministro}.png"
