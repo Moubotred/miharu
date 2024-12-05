1c747f9104a84e6ca3c087c50fc4fd62ab6d377b
Moubotred <fakesarnamer@gmail.com>
2024-12-04 19:32:59 -0500

    eliminacion de directorios

--- apis/test/__init__.py
+++
@@ -1,4 +0,0 @@
-import sys
-sys.path.append('/home/kimshizi/Proyects/miharu/')
-
-from utils import directorio
--- apis/utils/directorio.py
+++
@@ -1,6 +0,0 @@
-import asyncio
-from utils.syncro import syncro_temporal
-
-async def Directorio(suministro):
-    loop = asyncio.get_running_loop()
-    Directorio = await loop.run_in_executor(None, syncro_temporal,suministro)
