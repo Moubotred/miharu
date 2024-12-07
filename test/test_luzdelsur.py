bad64af7b34a04659ff766b1dd355b995ac1b95a
Moubotred <fakesarnamer@gmail.com>
2024-12-06 22:09:54 -0500

    sin modificaciones

--- test/test_luzdelsur.py
+++ test/test_luzdelsur.py
@@ -4,7 +4,7 @@ from apis.Luzdelsur import LuzdelsurRecibo
 @pytest.mark.asyncio
 async def test_LuzdelsurRecibo_simple():
     # Número de suministro de prueba
-    suministro = "7998"  # Este número debe ser válido en la API
+    suministro = "1274983"  # Este número debe ser válido en la API
     
     # Llamar a la función
     resultado = await LuzdelsurRecibo(suministro)
