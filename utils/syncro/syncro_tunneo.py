c462c048faf34e8d7af2b61ad37ea99e6337a6f6
Moubotred <fakesarnamer@gmail.com>
2024-12-07 18:59:03 -0500

    funcinalidad para tunnear el recibo con waifus y logo

--- utils/syncro/syncro_tunneo.py
+++ utils/syncro/syncro_tunneo.py
@@ -19,14 +19,30 @@ def recibo_anime(suministro):
     # Abrir la imagen base
     base_image = Image.open(f"{suministro}.png").convert("RGBA")
 
+    altura,ancho = base_image.size
+
+    posicion = {
+        '_resize':[400, 700],
+        '_posicion':[1090, 760],
+        '_resize_anime':[400, 330],
+        '_posicion_anime':[1120,60],
+        } if altura == 1653 else {
+        '_resize':[480,470],
+        '_posicion':[108,945],
+
+        '_resize_anime':[370,330],
+        '_posicion_anime':[100,5],
+
+        }
+
     # Crear la superposición (puede ser un rectángulo transparente con texto)
     overlay = Image.new("RGBA", base_image.size, (255, 255, 255, 0))
     draw = ImageDraw.Draw(overlay)
 
     logo_anime = waifu(tipo='sfw',categotia='neko')
     logo = Image.open(BytesIO(logo_anime.content)).convert("RGBA")
-    logo = logo.resize((400, 700))  # Redimensionar si es necesario
-    overlay.paste(logo, (1050, 760), logo)  # Pegar con transparencia
+    logo = logo.resize((posicion['_resize'][0],posicion['_resize'][1]))  # Redimensionar si es necesario
+    overlay.paste(logo, (posicion['_posicion'][0],posicion['_posicion'][1]), logo)  # Pegar con transparencia
 
     # Crear la superposición (puede ser un rectángulo transparente con texto)
     overlay_oficial = Image.new("RGBA", base_image.size, (255, 255, 255, 0))
@@ -35,8 +51,8 @@ def recibo_anime(suministro):
 
     logo_oficial = os.path.join('/home/kimshizi/Proyects/miharu/experimental','anime','fracazado.png') 
     logo = Image.open(logo_oficial).convert("RGBA")
-    logo = logo.resize((400, 330))  # Redimensionar si es necesario
-    overlay_oficial.paste(logo, (1120, 60), logo)  # Pegar con transparencia
+    logo = logo.resize((posicion['_resize_anime'][0],posicion['_resize_anime'][1]))  # Redimensionar si es necesario
+    overlay_oficial.paste(logo, (posicion['_posicion_anime'][0],posicion['_posicion_anime'][1]), logo)  # Pegar con transparencia
 
     # Combinar imágenes
     combined = Image.alpha_composite(base_image, overlay)
