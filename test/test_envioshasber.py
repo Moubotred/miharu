57e611e594f652f88ef3b5ceee3e4b6142890a53
Moubotred <fakesarnamer@gmail.com>
2024-12-08 13:32:22 -0500

    modificacion para el test

--- test/test_envioshasber.py
+++ test/test_envioshasber.py
@@ -4,11 +4,11 @@ from apis.Envioshasber import SistemaEnviosHasber
 @pytest.mark.asyncio
 async def test_hasber():
     # Número de suministro de prueba
-    suministro = "7798"  # Este número debe ser válido en la API
+    suministro = "9998"  # Este número debe ser válido en la API
     
     # Llamar a la función
     resultado = await SistemaEnviosHasber(suministro)
-
+    
     # Verificar si se devuelve el archivo esperado
     assert resultado.endswith('.pdf')
     # assert resultado == None
